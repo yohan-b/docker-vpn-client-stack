e8bd31664758265e0590f71e7becf8debe261826
yohan <783b8c87@scimetis.net>
2019-12-12 18:50:01 +0100

    Modified to be provide VPN access on a generic VM.

--- start_or_update.sh
+++ start_or_update.sh
@@ -11,30 +11,21 @@
 #       # setsebool -P domain_kernel_load_modules 1
 
 sudo modprobe ipt_MARK
-sudo modprobe l2tp_ppp
 MODULE_FILE=/etc/modules-load.d/docker.conf
 sudo bash -c "test -f $MODULE_FILE || touch $MODULE_FILE"
 sudo bash -c "grep -q ipt_MARK $MODULE_FILE \
 || { echo '# Loading ipt_MARK at boot is needed to use iptables -j MARK in docker containers' >> $MODULE_FILE; \
      echo 'ipt_MARK' >> $MODULE_FILE; }"
-sudo bash -c "grep -q l2tp_ppp $MODULE_FILE \
-|| { echo '# Loading l2tp_ppp at boot is needed to use xl2tpd with kernel drivers in docker containers' >> $MODULE_FILE; \
-     echo 'l2tp_ppp' >> $MODULE_FILE; }"
 
 test -z $1 || HOST="_$1"
 test -z $2 || INSTANCE="_$2"
 test -f ~/secrets.tar.gz.enc || curl -o ~/secrets.tar.gz.enc "https://cloud.scimetis.net/s/${KEY}/download?path=%2F&files=secrets.tar.gz.enc"
-openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | tar -zxv --strip 2 secrets/docker-VPN-client-FR-stack${HOST}${INSTANCE}/keys_privateinternetaccess secrets/docker-VPN-client-FR-stack${HOST}${INSTANCE}/users
+openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | sudo tar -zxv --strip 2 secrets/docker-VPN-client-stack${HOST}${INSTANCE}/keys
 
-sudo chown root:13 users
-sudo chown root:root entrypoint.sh
-sudo chmod +x entrypoint.sh
-sudo chown -R root. client_privateinternetaccess_FR.conf keys_privateinternetaccess
+sudo chown -R root. client.conf keys
 
 # --force-recreate is used to recreate container when crontab file has changed
-unset VERSION_PROXY VERSION_VPN_CLIENT VERSION_L2TP_CLIENT
-VERSION_PROXY=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-proxy.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_VPN_CLIENT=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-VPN-client.git| head -1 | cut -f 1|cut -c -10) \
-VERSION_L2TP_CLIENT=$(git ls-remote ssh://git@git.scimetis.net:2222/yohan/docker-l2tp-client.git| head -1 | cut -f 1|cut -c -10) \
+unset VERSION_VPN_CLIENT
+VERSION_VPN_CLIENT=$(git ls-remote https://git.scimetis.net/yohan/docker-VPN-client.git| head -1 | cut -f 1|cut -c -10) \
  sudo -E bash -c 'docker-compose up -d --force-recreate'
 
